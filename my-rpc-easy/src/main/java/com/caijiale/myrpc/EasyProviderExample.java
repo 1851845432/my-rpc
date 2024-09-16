2338b998afc4d0ae488b1f66f8fdfd24da485199
makabaka <1851845432@qq.com>
2024-09-16 16:46:32 +0800

    feat: Vertx 启动 监听

---
+++ my-rpc-easy/src/main/java/com/caijiale/myrpc/EasyProviderExample.java
@@ -0,0 +1,17 @@
+package com.caijiale.myrpc;
+
+
+import com.caijiale.myrpc.server.HttpServer;
+import com.caijiale.myrpc.server.VertxHttpServer;
+
+/**
+ * 简易服务提供者示例
+ */
+public class EasyProviderExample {
+
+    public static void main(String[] args) {
+        // 启动 web 服务
+        HttpServer httpServer = new VertxHttpServer();
+        httpServer.doStart(8080);
+    }
+}
