43d65dd4bfbff9fdfc41bd506a70ee8e4ed845d8
makabaka <1851845432@qq.com>
2024-09-17 18:22:06 +0800

    test: etcd 使用测试

---
+++ my-rpc-core/src/main/java/com/caijiale/myrpc/core/registry/EtcdRegistry.java
@@ -0,0 +1,37 @@
+package com.caijiale.myrpc.core.registry;
+
+import io.etcd.jetcd.ByteSequence;
+import io.etcd.jetcd.Client;
+import io.etcd.jetcd.KV;
+import io.etcd.jetcd.kv.GetResponse;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class EtcdRegistry {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        //日志级别
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "info");
+        // create client using endpoints
+        Client client = Client.builder().endpoints("http://localhost:2379")
+                .build();
+
+        KV kvClient = client.getKVClient();
+        ByteSequence key = ByteSequence.from("test_key".getBytes());
+        ByteSequence value = ByteSequence.from("test_value".getBytes());
+
+        // put the key-value
+        kvClient.put(key, value).get();
+
+        // get the CompletableFuture
+        CompletableFuture<GetResponse> getFuture = kvClient.get(key);
+
+        // get the value from CompletableFuture
+        GetResponse response = getFuture.get();
+        System.out.println(response.getKvs().get(0).getValue());
+
+        // delete the key
+        kvClient.delete(key).get();
+    }
+}
